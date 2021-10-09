2e82460adac17b4cbd5b97cc29845b3339373b41
Bhavybhoot <bhavy99989@gmail.com>
2021-10-08 22:54:00 -0700

    All Work Done

--- src/App.js
+++ src/App.js
@@ -1,26 +1,28 @@
-import React from 'react';
-import logo from './logo.svg';
-import './App.css';
+import React from "react";
+import "./App.css";
+import Navbar from "./components/Navbar";
+import { BrowserRouter as Router, Switch, Route } from "react-router-dom";
+import Home from "./components/pages/Home";
+import Products from "./components/pages/Products";
+import Services from "./components/pages/Services";
+import SignUp from "./components/pages/SignUp";
+import Footer from "./components/Footer";
 
 function App() {
-  return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
-  );
+	return (
+		<>
+			<Router>
+				<Navbar />
+				<Switch>
+					<Route path="/" exact component={Home} />
+					<Route path="/products" exact component={Products} />
+					<Route path="/services" exact component={Services} />
+					<Route path="/signup" exact component={SignUp} />
+				</Switch>
+				<Footer />
+			</Router>
+		</>
+	);
 }
 
 export default App;
